8512b8199d33cebf3d1e5a50deaf0591f7be3928
cesarabad <cesarabadvel@gmail.com>
2025-09-01 20:13:06 +0200

    feat(Command): Add abstract Command class header

--- Src/Core/Command/Command.h
+++ Src/Core/Command/Command.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <unordered_map>
+#include <memory>
+#include "../../Services/Security/SafetyChecker.h"
+#include "../../Services/Data/ConcurrentData.h"
+namespace Core::Commands {
+	class Command {
+	protected:
+		std::unordered_map<int, std::shared_ptr<SafetyChecker>> safetyCheckers_;
+		std::shared_ptr<ConcurrentData> data_;
+
+	public:
+		
+		Command(std::shared_ptr<ConcurrentData> data,
+			std::unordered_map<int, std::shared_ptr<SafetyChecker>> safetyCheckers = {})
+			: safetyCheckers_(std::move(safetyCheckers)), data_(std::move(data)) {}
+
+
+		virtual ~Command() = default;
+		virtual void execute() = 0;
+	};
+}
