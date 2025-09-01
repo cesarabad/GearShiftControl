9d135dbaf18d80e25963f987b06ff302510b2d49
cesarabad <cesarabadvel@gmail.com>
2025-09-01 20:27:18 +0200

    feat(ShafetyChecker): Add abstract ShafetyChecker class header

--- Src/Services/Security/SafetyChecker.h
+++ Src/Services/Security/SafetyChecker.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <memory>
+#include "../Data/ConcurrentData.h"
+
+namespace Services::Security {
+
+class SafetyChecker {
+
+protected:
+	std::shared_ptr<ConcurrentData> data_;
+
+public:
+	inline static const int CLUTCHED = 1;
+
+	SafetyChecker(std::shared_ptr<ConcurrentData> data) : data_(std::move(data)) {}
+	virtual ~SafetyChecker() = default;
+	virtual bool check() const = 0;
+	};
+
+}
