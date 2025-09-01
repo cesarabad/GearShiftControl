75179910fdd86026edac6a62c5ca6ac87859be9a
cesarabad <cesarabadvel@gmail.com>
2025-09-01 20:44:24 +0200

    refactor(SafetyChecker): Remove unnecesary members

--- Src/Services/Security/SafetyChecker.h
+++ Src/Services/Security/SafetyChecker.h
@@ -1,20 +1,13 @@
 #pragma once
-#include <memory>
-#include "../Data/ConcurrentData.h"
 
 namespace Services::Security {
 
 class SafetyChecker {
 
-protected:
-	std::shared_ptr<ConcurrentData> data_;
-
 public:
-	inline static const int CLUTCHED = 1;
-
-	SafetyChecker(std::shared_ptr<ConcurrentData> data) : data_(std::move(data)) {}
+	
 	virtual ~SafetyChecker() = default;
 	virtual bool check() const = 0;
-	};
+};
 
 }
