b4202f75ee591b16144b724599bb10ee3abb2656
cesarabad <cesarabadvel@gmail.com>
2025-09-02 10:20:14 +0200

    feat(InputOutputDevice): Add abstract InputOutputDevice class header

--- Src/Services/Device/InputOutput/InputOutputDevice.h
+++ Src/Services/Device/InputOutput/InputOutputDevice.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <cstdint>
+#include "../Input/InputDevice.h"
+#include "../Output/OutputDevice.h"
+
+namespace Services::Device {
+	class InputOutputDevice : public virtual InputDevice, public virtual OutputDevice {
+	public:
+		explicit InputOutputDevice(std::int32_t fd)
+			: Device(fd), InputDevice(), OutputDevice() {}
+
+		~InputOutputDevice() override = default;
+	};
+}
