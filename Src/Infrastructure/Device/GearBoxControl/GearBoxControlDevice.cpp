e942dc3788c45ced39de295b78cc88c06d995ee3
cesarabad <cesarabadvel@gmail.com>
2025-09-16 21:23:45 +0200

    fix(OutputDevice): make write() templated to allow type-safe data per device

--- Src/Infrastructure/Device/GearBoxControl/GearBoxControlDevice.cpp
+++ Src/Infrastructure/Device/GearBoxControl/GearBoxControlDevice.cpp
@@ -15,18 +15,9 @@ GearBoxControlDevice::GearBoxControlDevice(int device_serial_fd, const std::stri
 
 GearBoxControlDevice::~GearBoxControlDevice() = default;
 
-GearBoxControlDevice& GearBoxControlDevice::get_instance(int device_serial_fd, const std::string& device_function) {
-    if (!instance_) {
-        instance_ = std::unique_ptr<GearBoxControlDevice>(
-            new GearBoxControlDevice(device_serial_fd, device_function)
-        );
-    }
-    return *instance_;
-}
-
-void GearBoxControlDevice::write(const char* data, size_t size, Core::Model::GearModel::Gear gear) const {
-
+void GearBoxControlDevice::write(const Core::Model::GearModel::Gear& gear) const {
     std::string message = "X" + std::to_string(gear.rotation.x) +
         ",Y" + std::to_string(gear.rotation.y) + "\n";
+
     ::write(device_serial_fd_, message.c_str(), message.size());
 }
--- Src/Services/Device/Output/OutputDevice.h
+++ Src/Services/Device/Output/OutputDevice.h
@@ -2,9 +2,13 @@
 #include "../Device.h"
 
 namespace Services::Device {
-	class OutputDevice : public virtual Device {
-	public:
-		~OutputDevice() override = default;
-		virtual void write(const char* data, size_t size) const = 0;
-	};
+
+    template <typename T>
+    class OutputDevice : public virtual Device {
+    public:
+        ~OutputDevice() override = default;
+
+        virtual void write(const T& data) const = 0;
+    };
+
 }
