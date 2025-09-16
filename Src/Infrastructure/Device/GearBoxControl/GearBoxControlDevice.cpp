576e476982e406f69b6d9c0b87c712ea00e0a60a
cesarabad <cesarabadvel@gmail.com>
2025-09-16 21:44:20 +0200

    fix(GearBoxControlDevice): Fix write function

--- Src/Infrastructure/Device/GearBoxControl/GearBoxControlDevice.cpp
+++ Src/Infrastructure/Device/GearBoxControl/GearBoxControlDevice.cpp
@@ -15,10 +15,10 @@ GearBoxControlDevice::GearBoxControlDevice(int device_serial_fd, const std::stri
 
 GearBoxControlDevice::~GearBoxControlDevice() = default;
 
-void GearBoxControlDevice::write(const Core::Model::GearModel::Gear& gear) const {
-        instance_ = std::unique_ptr<GearBoxControlDevice>(
-            new GearBoxControlDevice(device_serial_fd, device_function)
-        );
-    }
-    return *instance_;
+void GearBoxControlDevice::write(Core::Model::GearModel::Gear gear) const {
+
+    std::string message = "X" + std::to_string(gear.rotation.x) +
+        ",Y" + std::to_string(gear.rotation.y) + "\n";
+
+    ::write(device_serial_fd_, message.c_str(), message.size());
 }
